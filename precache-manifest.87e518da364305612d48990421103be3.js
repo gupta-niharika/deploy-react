3a2f5273fcbe892c8c689500a3403629a9b1a8a8
Nikki <niharikap.gupta2019@vitstudent.ac.in>
2020-08-21 14:52:23 +0530

    Updates

---
+++ precache-manifest.87e518da364305612d48990421103be3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a6c0185a8054058218df7aad254065",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "f3ca5cca7b2bc1ef6493",
+    "url": "/deploy-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10e37635b8add09bdaa6",
+    "url": "/deploy-react/static/js/2.15ca8886.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.15ca8886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ca5cca7b2bc1ef6493",
+    "url": "/deploy-react/static/js/main.754d3a8d.chunk.js"
+  },
+  {
+    "revision": "94d9a03b33a4fc9eef8b",
+    "url": "/deploy-react/static/js/runtime-main.87e1f82a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
