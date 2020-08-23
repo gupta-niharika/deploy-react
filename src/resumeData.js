4a8edf5eff64f323dab98128fdc74795d1308444
Nikki <niharikap.gupta2019@vitstudent.ac.in>
2020-08-23 20:33:30 +0530

    website layout created

---
+++ src/resumeData.js
@@ -0,0 +1,51 @@
+let resumeData = {
+    "imagebaseurl":"https://gupta-niharika.github.io/deploy-react/",
+    "name": "Niharika Gupta",
+    "role": "A Backend Flask Developer",
+    "linkedinId":"https://www.linkedin.com/in/niharika-p-gupta/",
+    "roleDescription": "On my way to becoming a full stack developer...",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/niharika-p-gupta/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/gupta-niharika",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"Presently a 2nd year student at VIT Vellore. I do backend development in python flask, and I also design for fun. This is my first react app",
+    "address":"Bangalore, India",
+    "website":"https://gupta-niharika.github.io/deploy-react/", //hosted
+    "education":[
+      {
+        "UniversityName":"Vellore Institute of Technology",
+        "specialization":"Software Engineering",
+        "YearOfPassing":"2024",
+      },
+      
+    ],
+    "skillsDescription":"Few of my skills",
+    "skills":[
+      {
+        "skillname":"Python - Flask"
+      },
+      {
+        "skillname":"UI/UX Design"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ]    
+}
+  
+  export default resumeData
